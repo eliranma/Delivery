70cd1355d8859e55492a3f037dcde87b5dfd9de9
eliran <manzel@me.com>
2020-12-26 20:46:23 +0200

    add data json

--- data/menu.js
+++ data/menu.js
@@ -1,7 +1,93 @@
 export default [{
     categoryId:1,
+    categoryName:'ראשונות',
+    items:  [{
     itemId:1,
     shortDesc:'8 יחידות כנפיים ברוטב צ\'ילי - ברביקיו עם שומשום למעלה',
     itemName: 'כנפיים בצ\'ילי ברביקיו',
-    itemPrice: 25
-}]
+    itemPrice: 25,
+    posId:''
+    },
+    {
+    itemId:2,
+    shortDesc:'4 כדורי בשר מטוגנים בציפוי קריספי',
+    itemName: 'קריספי ביף ',
+    itemPrice: 32,
+    posId:''
+    },
+    {
+    itemId:3,
+    shortDesc:'4 כדורי בשר מטוגנים בציפוי קריספי',
+    itemName: 'קריספי ביף ',
+    itemPrice: 32,
+    posId:''
+    },
+    {
+    itemId:4,
+    shortDesc:'4 כדורי בשר מטוגנים בציפוי קריספי',
+    itemName: 'קריספי ביף ',
+    itemPrice: 32,
+    posId:''
+    },
+    {
+    itemId:5,
+    shortDesc:'4 כדורי בשר מטוגנים בציפוי קריספי',
+    itemName: 'קריספי ביף ',
+    itemPrice: 32,
+    posId:''
+    },
+    {
+    itemId:6,
+    shortDesc:'4 כדורי בשר מטוגנים בציפוי קריספי',
+    itemName: 'קריספי ביף ',
+    itemPrice: 32,
+    posId:''
+    }]
+    },
+    {
+    categoryId:2,
+    categoryName:'המבורגרים',
+    items:  [{
+        itemId:1,
+        shortDesc:'8 יחידות כנפיים ברוטב צ\'ילי - ברביקיו עם שומשום למעלה',
+        itemName: 'כנפיים בצ\'ילי ברביקיו',
+        itemPrice: 25,
+        posId:''
+        },
+        {
+        itemId:2,
+        shortDesc:'4 כדורי בשר מטוגנים בציפוי קריספי',
+        itemName: 'קריספי ביף ',
+        itemPrice: 32,
+        posId:''
+        },
+        {
+        itemId:3,
+        shortDesc:'4 כדורי בשר מטוגנים בציפוי קריספי',
+        itemName: 'קריספי ביף ',
+        itemPrice: 32,
+        posId:''
+        },
+        {
+        itemId:4,
+        shortDesc:'4 כדורי בשר מטוגנים בציפוי קריספי',
+        itemName: 'קריספי ביף ',
+        itemPrice: 32,
+        posId:''
+        },
+        {
+        itemId:5,
+        shortDesc:'4 כדורי בשר מטוגנים בציפוי קריספי',
+        itemName: 'קריספי ביף ',
+        itemPrice: 32,
+        posId:''
+        },
+        {
+        itemId:6,
+        shortDesc:'4 כדורי בשר מטוגנים בציפוי קריספי',
+        itemName: 'קריספי ביף ',
+        itemPrice: 32,
+        posId:''
+        }]
+    }
+]
