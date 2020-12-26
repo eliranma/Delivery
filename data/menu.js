483853b9274cbcab153b1a9e8c55d7efce73d812
eliran <manzel@me.com>
2020-12-26 20:36:20 +0200

    add data json

---
+++ data/menu.js
@@ -0,0 +1,7 @@
+export default [{
+    categoryId:1,
+    itemId:1,
+    shortDesc:'8 יחידות כנפיים ברוטב צ\'ילי - ברביקיו עם שומשום למעלה',
+    itemName: 'כנפיים בצ\'ילי ברביקיו',
+    itemPrice: 25
+}]
